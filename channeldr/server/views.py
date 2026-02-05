fcea8a58d3f41bda3c7ba93b718ef7e67a62e0ff
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-02-05 20:26:46 +0200

    api endpoint to filter by the the user logged in

--- channeldr/server/views.py
+++ channeldr/server/views.py
@@ -13,14 +13,22 @@ class ServerListViewSet(viewsets.ViewSet):
         # Sample data representing servers
         category = request.query_params.get('category')
         qty = request.query_params.get('qty') # default to 10 if not provided
+        by_user = request.query_params.get('by_user') == "true" 
         
         if category:
             # convert to JSON so it can be sent over to the frontend
             # done using a serializer
             self.queryset = self.queryset.filter(category__name=category) #updating queryset to filter by category name
             
+        
+        if by_user:
+            user_id = request.user.id
+            self.queryset = self.queryset.filter(member=user_id) # updating queryset to filter by user ID
+
+
         if qty:
             self.queryset = self.queryset[:int(qty)] # updating queryset to limit the number of results
+
 
         serializer = ServerSerializer(self.queryset, many=True)
         return Response(serializer.data)
