ddf320c55a476a259a8ef63b240f2d05877b4d01
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-02-05 20:35:19 +0200

    addition of comment to views

--- channeldr/server/views.py
+++ channeldr/server/views.py
@@ -14,17 +14,19 @@ class ServerListViewSet(viewsets.ViewSet):
         category = request.query_params.get('category')
         qty = request.query_params.get('qty') # default to 10 if not provided
         by_user = request.query_params.get('by_user') == "true" 
+
+        '''
+        order of if statements is the order of appearents in search bar
+        '''
         
         if category:
             # convert to JSON so it can be sent over to the frontend
             # done using a serializer
             self.queryset = self.queryset.filter(category__name=category) #updating queryset to filter by category name
-            
-        
+                    
         if by_user:
             user_id = request.user.id
             self.queryset = self.queryset.filter(member=user_id) # updating queryset to filter by user ID
-
 
         if qty:
             self.queryset = self.queryset[:int(qty)] # updating queryset to limit the number of results
