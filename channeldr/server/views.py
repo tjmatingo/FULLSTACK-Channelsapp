8ab91e51db55cb57a6af93a0789bdd59fda1bd17
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-02-06 16:46:05 +0200

    api endpoint to filter by the the server ID

--- channeldr/server/views.py
+++ channeldr/server/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError, AuthenticationFailed
 
 from .models import Server
 from .serializer import ServerSerializer
@@ -14,10 +15,13 @@ class ServerListViewSet(viewsets.ViewSet):
         category = request.query_params.get('category')
         qty = request.query_params.get('qty') # default to 10 if not provided
         by_user = request.query_params.get('by_user') == "true" 
-
+        by_serverID = request.query_params.get('by_serverID')
         '''
         order of if statements is the order of appearents in search bar
         '''
+
+        if by_user or by_serverID and not request.user.is_authenticated:
+            raise AuthenticationFailed()
         
         if category:
             # convert to JSON so it can be sent over to the frontend
@@ -28,9 +32,19 @@ class ServerListViewSet(viewsets.ViewSet):
             user_id = request.user.id
             self.queryset = self.queryset.filter(member=user_id) # updating queryset to filter by user ID
 
+        
+
         if qty:
             self.queryset = self.queryset[:int(qty)] # updating queryset to limit the number of results
 
+
+        if by_serverID:
+            try:
+                self.queryset = self.queryset.filter(id=by_serverID)
+                if not self.queryset.exists():
+                    raise ValidationError(detail=f"Server with id {by_serverID} not found")
+            except ValueError: 
+                raise ValidationError(detail=f"Server ValueError")
 
         serializer = ServerSerializer(self.queryset, many=True)
         return Response(serializer.data)
