dbd661d08fc3e4d1bf78bde8ba6a6bc12709f993
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-02-07 20:56:33 +0200

    adding banner to channel

--- channeldr/server/models.py
+++ channeldr/server/models.py
@@ -3,6 +3,13 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from django.dispatch import receiver
 
+def serverIconUploadPath(instance, filename):
+    return f'server/{instance.id}/server_icons/{filename}'
+
+def serverBannerUploadPath(instance, filename):
+    return f'server/{instance.id}/server_banner/{filename}'
+
+# function for the directory path for saving category icons
 def categoryIconUploadPath(instance, filename):
     return f"category/{instance.id}/category_icon/{filename}"
 
@@ -54,6 +61,8 @@ class Channel(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='channel_owner')
     server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name='channel_server')
     topic = models.CharField(max_length=100)
+    banner = models.ImageField(upload_to=serverBannerUploadPath, null=True, blank=True)
+    icon = models.ImageField(upload_to=serverIconUploadPath, null=True, blank=True)
 
 
     def save(self, *args, **kwargs):
@@ -62,6 +71,23 @@ class Channel(models.Model):
         self.name = self.name.lower()
         super(Channel, self).save(*args, **kwargs)
 
+        # checking if the id exists
+        if self.id:
+            existing = get_object_or_404(Channel, id=self.id)
+
+            # if icon is changed the old one must be deleted
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+        super(Channel, self).save(*args, **kwargs)
+    
+    # if channel is deleted icon must be deleted as well
+    @receiver(models.signals.pre_delete, sender="server.Channel")
+    def category_delete_files(sender, instance, **kwargs):        
+        for field in instance._meta.fields:
+            if field.name == "icon":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
 
 
 
