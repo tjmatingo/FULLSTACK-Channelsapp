8cc29b520565b1e33ef8bfc703a174a1ca67a7b5
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-02-09 21:53:45 +0200

    integration of material ui

--- channelrf/src/pages/Home.tsx
+++ channelrf/src/pages/Home.tsx
@@ -1,12 +1,14 @@
+import { CssBaseline } from '@mui/material';
+import Box from '@mui/material/Box'
+
 const Home = () => {
 
     return (
-        <>
-        <h1>Home</h1>
-
-        </>
-    );
+        <Box sx={{ display: "flex" }}>
+            <CssBaseline />
+            Home
+        </Box>
+    )
 };
-
 
 export default Home;
