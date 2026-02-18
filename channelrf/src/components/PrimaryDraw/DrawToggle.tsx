9bde89c594ca0268644e938c1f5271b5d8e6e7d3
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-02-18 20:40:19 +0200

    drawer button

---
+++ channelrf/src/components/PrimaryDraw/DrawToggle.tsx
@@ -0,0 +1,15 @@
+import { DisabledByDefault } from "@mui/icons-material"
+import { Box, IconButton } from "@mui/material"
+
+ const DrawToggle = () => {
+    return (
+        <Box sx={{height: "50px", display: "flex", alignItems: "center", justifyContent: "center",}}>
+            <IconButton>
+
+            </IconButton>
+
+        </Box>
+    );
+ };
+
+ export default DrawToggle;
