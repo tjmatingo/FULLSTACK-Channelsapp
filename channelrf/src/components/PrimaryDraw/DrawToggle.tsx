827ad76b60eb08b4b10fe4a574a487bbd0b6d9a8
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-02-20 21:42:10 +0200

    props setup for drawer toggle button

--- channelrf/src/components/PrimaryDraw/DrawToggle.tsx
+++ channelrf/src/components/PrimaryDraw/DrawToggle.tsx
@@ -1,6 +1,14 @@
 import { Box, IconButton } from "@mui/material"
 import  ChevronLeft from "@mui/icons-material/ChevronLeft";
 
+
+type Props = {
+    open: boolean;
+    handleDrawerOpen: () => void;
+    handleDrawerClosed: () => void;
+}
+
+
  const DrawToggle = () => {
     return (
         <Box sx={{height: "50px", display: "flex", alignItems: "center", justifyContent: "center",}}>
