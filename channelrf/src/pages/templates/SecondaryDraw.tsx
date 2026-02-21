106433854dc652c412d269430a8633ded28b7bd4
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-02-21 22:05:11 +0200

    secondary drawer fixes to overflow and width

--- channelrf/src/pages/templates/SecondaryDraw.tsx
+++ channelrf/src/pages/templates/SecondaryDraw.tsx
@@ -6,11 +6,12 @@ const SecondaryDraw = () => {
     
     return (
         <Box sx={{
-            minWidth: `${theme.secondaryDraw.width}`, 
+            minWidth: `${theme.secondaryDraw.width}px`, 
             height: `calc(100vh - ${theme.primaryAppBar.height}px )`, 
             mt: `${theme.primaryAppBar.height}px`,
             borderRight: `1px solid ${theme.palette.divider}`,
             display: {xs: "none", sm: "block"},
+            overflow: "auto",
         }}>
 
         
