53d4d22eebb649d5a6dab5ffec9c0b0e7c7323d2
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-02-23 18:56:58 +0200

    testing with an array of 1 - 100

--- channelrf/src/pages/templates/Main.tsx
+++ channelrf/src/pages/templates/Main.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { useTheme } from "@mui/material";
 
 const Main = () => {
@@ -13,6 +13,12 @@ const Main = () => {
             // to allow scrolling independly of every other component
             overflow: "hidden",
         }}>
+            
+            {[...Array(100)].map((_, i) => (
+                <Typography key={i} paragraph>
+                    {i + 1}
+                </Typography>
+            ))}
 
         </Box>
 
