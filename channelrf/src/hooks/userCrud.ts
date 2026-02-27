6daebf817a60a81b708ff607be4b84a79e03b48e
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-02-27 21:16:01 +0200

    crud hook loading fetched data and error handling

--- channelrf/src/hooks/userCrud.ts
+++ channelrf/src/hooks/userCrud.ts
@@ -13,17 +13,24 @@ const useCrud  = <T>(initialData: T[], apiURL: string): IuseCrud<T> => {
     const jwtAxios = useAxiosWithInterceptor();
     const [dataCrud, setDataCrud] = useState<T[]>(initialData)
     const [error, setError] = useState<Error | null>(null)
+    const [isLoading, setIsLoading] = useState(false)
 
     const fetchData = async () => {
-        
+        setIsLoading(true)
         // sending request using interceptor
         try{
             const response = await jwtAxios.get('${BASE_URL}${apiURL}', {})
             const data = response.data
             setDataCrud(data)
             setError(null)
+            setIsLoading(false)
+            return data;
             
-        }catch(error: any)
+        } catch(error: any){
+            if (error.response && error.response.status === 400) {
+                setError(new Error("400"))
+            }
+        }
     };
     
     return {fetchData}
