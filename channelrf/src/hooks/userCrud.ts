c15444d2dcd66c7d04ca4153eee04ed7a86b129e
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-02-27 21:11:01 +0200

    crud hook error handling setup

--- channelrf/src/hooks/userCrud.ts
+++ channelrf/src/hooks/userCrud.ts
@@ -1,11 +1,29 @@
 import useAxiosWithInterceptor from "../helpers/jwtInterceptor";
 import { BASE_URL } from "../config";
+import { useState } from "react";
 
-const useCrud  = () => {
+interface IuseCrud<T> {
+    dataCrud: T[];
+    fetchData: () => Promise<void>;
+    error: Error | null;
+}
+
+
+const useCrud  = <T>(initialData: T[], apiURL: string): IuseCrud<T> => {
     const jwtAxios = useAxiosWithInterceptor();
+    const [dataCrud, setDataCrud] = useState<T[]>(initialData)
+    const [error, setError] = useState<Error | null>(null)
 
-    const fetchData = () => {
+    const fetchData = async () => {
+        
         // sending request using interceptor
+        try{
+            const response = await jwtAxios.get('${BASE_URL}${apiURL}', {})
+            const data = response.data
+            setDataCrud(data)
+            setError(null)
+            
+        }catch(error: any)
     };
     
     return {fetchData}
