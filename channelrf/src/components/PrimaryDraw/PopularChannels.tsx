e770d55547346e8ec6f8719a4d183e7d07bedd82
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-02-28 21:42:04 +0200

    popular channel setup

---
+++ channelrf/src/components/PrimaryDraw/PopularChannels.tsx
@@ -0,0 +1,15 @@
+import { Box, Typography, List, ListItem, ListItemButton, ListItemIcon, ListItemText  } from "@mui/material";
+import useCrud from "../../hooks/userCrud";
+import { useEffect } from "react";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import { MEDIA_URL } from "../../config";
+import { Link } from "react-router-dom";
+
+
+const PopularChannel = () => {
+    return ();
+};
+
+
+export default PopularChannel;
