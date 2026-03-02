309d245a52ef1e75dca79a0d16b287c86e850365
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-02 22:44:46 +0200

    serializer edit to show category name not id

--- channeldr/server/serializer.py
+++ channeldr/server/serializer.py
@@ -14,6 +14,7 @@ class ChannelSerializer(serializers.ModelSerializer):
 class ServerSerializer(serializers.ModelSerializer):
     num_members = serializers.SerializerMethodField()
     channel_server = ChannelSerializer(many=True)
+    category = serializers.StringRelatedField()
     
     # selecting server db table and excluding the member field
     class Meta:
