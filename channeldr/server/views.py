82ef6d69bef46f7da16beb20e5799bfc8e86e190
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-02 21:01:20 +0200

    authentication bypass isAuthenticated issue

--- channeldr/server/views.py
+++ channeldr/server/views.py
@@ -12,7 +12,7 @@ from .serializer import ServerSerializer
 # endpoint setup
 class ServerListViewSet(viewsets.ViewSet):
     queryset = Server.objects.all()
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
 
     @server_list_docs
     def list(self, request):
