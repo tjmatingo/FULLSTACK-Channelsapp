9fff98ecb550edd3fd958b273f68579526c97c10
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-05 21:00:02 +0200

    categories component setup

---
+++ channelrf/src/components/SecondaryDraw/ExploreCategories.tsx
@@ -0,0 +1,16 @@
+import { Box, Typography, List, ListItem, ListItemButton, ListItemText, useTheme, ListItemIcon } from "@mui/material";
+import useCrud from "../../hooks/userCrud";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import { MEDIA_URL } from "../../config";
+
+
+const ExploreCategories = () => {
+    return ( 
+        <>
+        </>
+    );
+};
+
+export default ExploreCategories;
